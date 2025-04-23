84979e364b4e5a5baa357b436cb16272ae0fdc8b
jhssic <jhessicamariaaf@gmail.com>
2025-04-23 20:00:51 -0300

    feat: add frontend dependecies

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
